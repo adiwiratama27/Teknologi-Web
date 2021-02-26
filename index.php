057ffeb6edcff6e3edbd0070a66205534896918f
adiwiratama27 <79655177+adiwiratama27@users.noreply.github.com>
2021-02-26 10:21:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Welcome to Web Adi</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="with=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image,preview:large, max-video-preview:-1">
+    <link rel="cannonical" href="" />
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:tittle" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <a href ="#"> Tentang
+
+</head>
+<body>
+<div class="jumbotron text-center">
+        <h1>Selamat Datang di Toko Slimbag</h1>
+        <img src="media\slimbag.jpg" width = "500">
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+               <center> <h3> Tentang </h3> <center>
+            </div>
+            <div class="col-sm-4">
+                <center> <h3>Warna</h3> <center>
+            </div>
+            <div class="col-sm-4">
+               <center> <h3>Comment</h3> <center>
+            </div>
+            <div class="col-sm-4">
+              <center> <h3>Lebih Banyak</h3> <center>
+            </div>
+            <div class="col-sm-4">
+               <center> <h3>Merk<h3> <center>
+            </div>
+        </div>
+    </div>
+    <a href = s
+</body>
+</html>
