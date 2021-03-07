0ed2e9f7e0488234f754387e1de26af9c91700f0
adiwiratama27 <79655177+adiwiratama27@users.noreply.github.com>
2021-03-07 14:04:37 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,50 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Welcome to Web Adi</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="with=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="robots" content="index, follow, max-snippet:-1, max-image,preview:large, max-video-preview:-1">
-    <link rel="cannonical" href="" />
-    <meta property="og:locale" content="en_US">
-    <meta property="og:type" content="website">
-    <meta property="og:tittle" content="">
-    <meta property="og:description" content="">
-    <meta property="og:url" content="">
-    <meta property="og:site_name" content="">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <a href ="#"> Tentang
-
+  <title>WELCOME TO WEB ADI</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-<div class="jumbotron text-center">
-        <h1>Selamat Datang di Toko Slimbag</h1>
-        <img src="media\slimbag.jpg" width = "500">
-    </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4">
-               <center> <h3> Tentang </h3> <center>
-            </div>
-            <div class="col-sm-4">
-                <center> <h3>Warna</h3> <center>
-            </div>
-            <div class="col-sm-4">
-               <center> <h3>Comment</h3> <center>
-            </div>
-            <div class="col-sm-4">
-              <center> <h3>Lebih Banyak</h3> <center>
-            </div>
-            <div class="col-sm-4">
-               <center> <h3>Merk<h3> <center>
-            </div>
-        </div>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">
+    <img src="media/logo.jpg" alt="logo" style="width:40px;">
+  </a>
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">HOME</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="gambar.html">WARNA</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="komen.html">COMMENT</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="form.html">SIGN UP</a>
+    </li>
+  </ul>
+</nav>
+<div class="container">                 
+  <div class="spinner-grow text-muted"></div>
+  <div class="spinner-grow text-primary"></div>
+  <div class="spinner-grow text-success"></div>
+  <div class="spinner-grow text-info"></div>
+  <div class="spinner-grow text-warning"></div>
+  <div class="spinner-grow text-danger"></div>
+  <div class="spinner-grow text-secondary"></div>
+  <div class="spinner-grow text-dark"></div>
+  <div class="spinner-grow text-light"></div>
+</div>
+<div class="jumbotron text-center">
+        <h1>WELCOME TO SLIMBAG STORE</h1>
+        <img src="media\slimbag.jpg" width="400">
     </div>
-    <a href = s
 </body>
 </html>
